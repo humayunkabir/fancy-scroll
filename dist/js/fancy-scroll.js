3f680a7e99eb8fd472cb480efcf6afc48222ab60
Naem Bhuiyan <hopingboynaem@gmail.com>
2019-05-05 12:32:33 +0600

    v0.1.2

--- dist/js/fancy-scroll.js
+++ dist/js/fancy-scroll.js
@@ -4,11 +4,16 @@
 |   On page scroll for #id targets
 -----------------------------------------------*/
 $(document).ready(function ($) {
+  var _window = window,
+      location = _window.location,
+      history = _window.history;
   $('a[data-fancyscroll]').click(function scrollTo(e) {
-    // const $this = $(e.currentTarget);
     var $this = $(this);
+    var pathname = location.pathname,
+        hostname = location.hostname;
+    var condition = pathname === $this[0].pathname && pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && hostname === this.hostname;
 
-    if (window.location.pathname === $this[0].pathname && window.location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && window.location.hostname === this.hostname) {
+    if (condition) {
       e.preventDefault();
       var target = $(this.hash);
       target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
@@ -18,7 +23,7 @@ $(document).ready(function ($) {
           scrollTop: target.offset().top - ($this.data('offset') || 0)
         }, 400, 'swing', function () {
           var hash = $this.attr('href');
-          window.history.pushState ? window.history.pushState(null, null, hash) : window.location.hash = hash;
+          history.pushState ? history.pushState(null, null, hash) : location.hash = hash;
         });
         return false;
       }
@@ -26,14 +31,14 @@ $(document).ready(function ($) {
 
     return true;
   });
-  var hash = window.location.hash;
+  var hash = location.hash;
 
   if (hash && document.getElementById(hash.slice(1))) {
     var $this = $(hash);
     $('html,body').animate({
       scrollTop: $this.offset().top - $("a[href='" + hash + "']").data('offset')
     }, 400, 'swing', function () {
-      window.history.pushState ? window.history.pushState(null, null, hash) : window.location.hash = hash;
+      history.pushState ? history.pushState(null, null, hash) : location.hash = hash;
     });
   }
 });
